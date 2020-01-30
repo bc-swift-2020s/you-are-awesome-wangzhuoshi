2475a8b65fc663cf0ff36e3bc077b9020c277c0f
Wangzhuo Shi <shiwj@bc.edu>
2020-01-29 19:45:01 -0500

    2.14 Yeahh Completed

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -7,15 +7,18 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     // let awesomeMessage = "You Are Awesome!"
-   // let greateMesage = "You Are Great!"
-   // let bombMessage = "You Are Da Bomb!"
+    // let greateMesage = "You Are Great!"
+    // let bombMessage = "You Are Da Bomb!"
     var imageNumber = 0
     var messageNumber = 0
     let totalNumberOfImages = 9
+    var audioPlayer = AVAudioPlayer()
+    
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +29,7 @@ class ViewController: UIViewController {
         let message = ["You Are Awesome!", "You Are Great!", "You Are Fantastic!", "Fabulous? That's You!"]
         var newMessageNumber : Int
         repeat  {
-               newMessageNumber = Int.random(in: 0...message.count-1)
+            newMessageNumber = Int.random(in: 0...message.count-1)
         } while (messageNumber == newMessageNumber)
         messageNumber = newMessageNumber
         messageLabel.text = message[messageNumber]
@@ -37,34 +40,46 @@ class ViewController: UIViewController {
         } while (imageNumber == newImageNumber)
         imageNumber = newImageNumber
         imageView.image = UIImage(named: "image\(imageNumber)")
+        
+        if let sound = NSDataAsset(name: "sound0"){
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("Error :\(error.localizedDescription) Could not ionitialize AVAudioplayer object") }
+        } else  {
+                print ("Error: could not read data from the file")
+            }
+        }
+        
         //messageLabel.text = message[messageNumber]
         //messageNumber += 1
         //if messageNumber == message.count {
-          //  messageNumber = 0
-       // }
+        //  messageNumber = 0
+        // }
         
         //print (imageNumber)
-       // let imageName = "image\(imageNumber)"
+        // let imageName = "image\(imageNumber)"
         //imageView.image = UIImage(named: imageName)
         //imageNumber += 1
         //if imageNumber == 10 {
-          //  imageNumber = 0
+        //  imageNumber = 0
         //}
         
-       // if messageLabel.text == awesomeMessage {
-         //   messageLabel.text = greateMesage
-           // imageView.image = UIImage(named:"image1")
+        // if messageLabel.text == awesomeMessage {
+        //   messageLabel.text = greateMesage
+        // imageView.image = UIImage(named:"image1")
         //}else if messageLabel.text == greateMesage {
-          //  messageLabel.text = bombMessage
-            //imageView.image = UIImage(named:"image2")
+        //  messageLabel.text = bombMessage
+        //imageView.image = UIImage(named:"image2")
         //} else {
-          //  messageLabel.text = awesomeMessage
-            //imageView.image = UIImage(named:"image0")
-            
+        //  messageLabel.text = awesomeMessage
+        //imageView.image = UIImage(named:"image0")
+        
         //}
     }
     
     
     
-}
+
 
